e3498b349be9113ead39e72e7fcc32488a30355b
Sergey Ivanov <ivanitch.mail@gmail.com>
2021-04-02 09:32:36 +0900

    Простой список заметок

--- app.js
+++ app.js
@@ -1,8 +1,36 @@
 const App = {
   data() {
     return {
-      counter: 0,
-      title: 'Счётчик'
+      placeholder: 'Название заметки',
+      title: 'Список заметок',
+      inputValue: '',
+      notes: ['note 1', 'note 2'],
+    }
+  },
+  methods: {
+    addNote () {
+      if (this.inputValue !== '') {
+        this.notes.push(this.inputValue)
+        this.inputValue = ''
+      }
+
+    },
+    deleteNote (idx, event) {
+      this.notes.splice(idx, 1)
+      //console.log(event)
+    },
+    upperCase (item) {
+      return item.toUpperCase()
+    }
+  },
+  computed: {
+    doubleCount () {
+      return this.notes.length * 2
+    }
+  },
+  watch: {
+    inputValue (value) {
+      console.log(value)
     }
   }
 }
